d561bd5f9235816397aac198d8e3f9009970094e
vinee <vineethserigar17@gmail.com>
2021-07-22 17:52:52 +0530

    added Images from API

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useParams } from "react-router";
 import MovieNavbar from "../components/Navbar/movieNavbar.component";
+import { MovieContext } from "../context/Movie.context";
 
 const Movielayout = (props) => {
+  const { id } = useParams();
   return (
     <>
       <MovieNavbar />
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
 import PosterSlider from "../components/PosterSlide/PosterSlider.component";
 
-import TempPosters from "../config/TempPosters.config";
-
 const HomePage = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+  useEffect(() => {
+    const requstPopularMovies = async () => {
+      const getPopularMovies = await axios.get("/movie/popular");
+      setPopularMovies(getPopularMovies.data.results);
+    };
+    requstPopularMovies();
+  }, []);
+
+  useEffect(() => {
+    const requestTopRatedMovies = async () => {
+      const getTopRatedMovies = await axios.get("/movie/top_rated");
+      setTopRatedMovies(getTopRatedMovies.data.results);
+    };
+    requestTopRatedMovies();
+  }, []);
+
+  useEffect(() => {
+    const requestUpcomingMovies = async () => {
+      const getUpcomingMovies = await axios.get("/movie/upcoming");
+      setUpcomingMovies(getUpcomingMovies.data.results);
+    };
+    requestUpcomingMovies();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-12">
@@ -25,7 +52,7 @@ const HomePage = () => {
               />
             </div>
             <PosterSlider
-              images={TempPosters}
+              images={popularMovies}
               title="Premiers"
               subtitle="Brand new releases every Friday"
               isDark
@@ -35,14 +62,14 @@ const HomePage = () => {
       </div>
       <div className="container mx-auto px-4 my-10">
         <PosterSlider
-          images={TempPosters}
+          images={topRatedMovies}
           title="Online Streaming Events"
           isDark={false}
         />
       </div>
       <div className="container mx-auto px-4 my-10">
         <PosterSlider
-          images={TempPosters}
+          images={upcomingMovies}
           title="Outdoor Events"
           isDark={false}
         />
